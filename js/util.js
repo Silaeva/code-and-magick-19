2c1607b546a981895ba3647da5b7923733bd771a
Anna <tkachenko481@mail.ru>
2020-02-25 19:48:03 +0300

    Добавляет загрузку данных с сервера

--- js/util.js
+++ js/util.js
@@ -1,14 +1,30 @@
 'use strict';
 
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
+(function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
 
-var getRandomElement = function (data) {
-  return data[Math.floor(Math.random() * data.length)];
-};
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
+  };
 
-window.util = {
-  ESC_KEYCODE: ESC_KEYCODE,
-  ENTER_KEYCODE: ENTER_KEYCODE,
-  getRandomElement: getRandomElement
-};
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  };
+
+  var getRandomElement = function (data) {
+    return data[Math.floor(Math.random() * data.length)];
+  };
+
+  window.util = {
+    ESC_KEYCODE: ESC_KEYCODE,
+    ENTER_KEYCODE: ENTER_KEYCODE,
+    getRandomElement: getRandomElement,
+    isEscEvent: isEscEvent,
+    isEnterEvent: isEnterEvent
+  };
+})();
