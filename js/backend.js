9abce913fafe7856847840be85d87e8d0accbea3
Anna <tkachenko481@mail.ru>
2020-02-29 20:39:06 +0300

    Описывает работу с сервером

--- js/backend.js
+++ js/backend.js
@@ -1,8 +1,8 @@
 'use strict';
 
 (function () {
-  var URL_POST = 'https://js.dump.academy/code-and-magick'; // save
-  var URL_GET = 'https://js.dump.academy/code-and-magick/data'; // load
+  var URL_POST = 'https://js.dump.academy/code-and-magick';
+  var URL_GET = 'https://js.dump.academy/code-and-magick/data';
   var StatusCode = {
     OK: 200
   };
